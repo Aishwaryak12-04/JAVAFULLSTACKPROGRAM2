1242181aa054cff5837724e619e03072047d3b02
Aishwaryak12-04 <aishwaryak1204@gmail.com>
2024-08-01 15:52:24 +0530

    Add EMSPROJECT as a regular directory

---
+++ EMSPROJECT/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Employee } from './model/Employee';
+
+@Component({
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrl: './app.component.css',
+})
+export class AppComponent {
+title = 'empManagment';
+employee: Employee;
+constructor() {
+this.employee = new Employee();
+}
+insertEmployee(data: any) {
+this.employee.empId = data.empId;
+this.employee.empName = data.empName;
+this.employee.empId = data.empId;
+}
+}
