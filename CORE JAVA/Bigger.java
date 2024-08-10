b5ab5effe64ba4babd8e87ef4946162e766d273d
Aishwaryak12-04 <aishwaryak1204@gmail.com>
2024-08-10 12:00:05 +0530

    Bigger

---
+++ CORE JAVA/Bigger.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Bigger {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 10;
+
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("b is greater than or equal to a");
+        }
+    }
+}
